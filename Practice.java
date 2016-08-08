9e24d88f4571c8026883a8021c6eb970d41e82f0
Bharathi1310 <bharathikalai1310@gmail.com>
2016-08-08 00:08:29 -0700

    Create Practice.java

---
+++ Practice.java
@@ -0,0 +1,22 @@
+# bha
+import java.util.Scanner;
+
+
+public class Practice {
+
+	public static void main(String[] args) {
+     Scanner in=new Scanner(System.in);
+     System.out.println("enter the integer : ");
+     int num=in.nextInt();
+     int mod;
+     int sum=0;
+     while(num!=0){
+    	 mod=num%10;
+    	 sum=sum*10+mod;
+    	 num=num/10;
+    	 
+     }
+     System.out.println();
+	}
+
+}
